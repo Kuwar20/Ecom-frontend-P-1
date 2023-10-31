baf3b02374b5daae2838e938aea8bc2c873fdb33
Kuwar20 <kuwarx1@gmail.com>
2023-10-31 20:40:24 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box"> Description </div>
+                <div className="descriptionbox-nav-box fade"> Reviews (122) </div>
+
+            </div>
+            <div className="descriptionbox-description">
+                <p>Welcome to our premier online fashion emporium! Dive into a world of style, elegance, and comfort as you explore our wide selection of clothing for men, women, and children. Our user-friendly platform offers an extensive range of trendy and timeless outfits, from casual wear to formal attire. With a commitment to quality and fashion, we provide an effortless shopping experience, ensuring that you're always dressed to impress. Discover the latest trends, classic essentials, and everything in between, right here at your fingertips.</p>
+                <p>Welcome to your ultimate fashion destination. Explore an array of clothing for everyone - men, women, and children. Our online store is your one-stop-shop for trendy and timeless fashion. With a seamless shopping experience, discover the latest styles, from everyday comfort to elegant attire. Find your perfect look effortlessly.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
